2295e39f8109808e0679203279dbf01e3b594563
aaronhsueh <aaronhsueh66@gmail.com>
2025-03-17 21:45:01 +0800

    Added new parts to Sorting playground

--- IGCSE Comp Sci/Sorting playground.py
+++ IGCSE Comp Sci/Sorting playground.py
@@ -1,1 +1,5 @@
-print((lambda n: f"Min: {min(n)}, Max: {max(n)}")([int(input(f"Enter number {i+1}: ")) for i in range(5)]))
+# First attempt:
+# print((lambda n: f"Min: {min(n)}, Max: {max(n)}")([int(input(f"Enter number {i+1}: ")) for i in range(5)]))
+
+# Second attempt:
+print((lambda n: f"Min: {min(n)}, Max: {max(n)}")([int(input(f"Enter number {i+1}: ")) for i in range(int(input("How many numbers? ")))]))
